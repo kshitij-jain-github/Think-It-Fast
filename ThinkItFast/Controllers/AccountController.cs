22a6b37b110fbdf7496bb2f518b9d1129d06b3d9
Kshitij Jain <jainkshitij1530@gmail.com>
2023-09-06 12:51:13 +0530

    BACKEND CONTROLLER CONNECTION

---
+++ ThinkItFast/Controllers/AccountController.cs
@@ -0,0 +1,210 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ThinkItFast.Extensions;
+using ThinkItFast.Models;
+using ThinkItFast.Repository.Interfaces;
+using ThinkItFast.ViewModel;
+
+namespace ThinkItFast.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ILogger<AccountController> _logger;
+        private readonly ICandidate< Candidate> _candidate;
+
+        public AccountController(ILogger<AccountController> logger, ICandidate< Candidate> Candidate)
+        {
+            _candidate = Candidate;
+            _logger = logger;
+        }
+
+        // GET: AccountController
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Register()
+        {
+            if (TempData["Message"] != null)
+            {
+                TempData["Message"] = null;
+            }
+            return PartialView("_Register");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register([FromForm] RegisterViewModel objCollection)
+        {
+            int i = 0;
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    IQueryable<Candidate> _iqCandidate = await _candidate.SearchCandidate(e => e.Candidate_ID.Equals(objCollection.Candidate_ID));
+                    if (!_iqCandidate.Any())
+                    {
+                         Candidate _objcandidate = new  Candidate()
+                        {
+                            Name = objCollection.Name,
+                            Email = objCollection.Email,
+                            Phone = objCollection.Phone,
+                            Candidate_ID = objCollection.Candidate_ID,
+                            Roles = "User",
+                            Password = objCollection.Password.EncodeBase64(),
+                            CreatedBy = "SYSTEM",
+                            CreatedOn = DateTime.Now
+                        };
+
+                        i = await _candidate.AddCandidate(_objcandidate);
+
+                        if (i > 0)
+                            return RedirectToAction("Login", "Account");
+                        else
+                            TempData["Message"] = "An error occurred.";
+                    }
+                    else
+                        TempData["Message"] = "A user already exists with that Candidate ID.";
+                }
+                catch (Exception ex)
+                {
+                    TempData["Message"] = ex.Message;
+                    throw new Exception(ex.Message, ex.InnerException);
+                }
+            }
+            return PartialView("_Register");
+        }
+
+        // GET: AccountController
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Login()
+        {
+            try
+            {
+                string _Action = string.Empty;
+                string _Controller = string.Empty;
+                string value = Convert.ToString(HttpContext.Session.GetString("AuthenticatedUser"));
+
+                if (string.IsNullOrEmpty(value))
+                    return PartialView("_Login");
+                else
+                    return RedirectToAction("Index", "Home");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message, ex.InnerException);
+            }
+            finally
+            {
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login([FromForm] LoginViewModel objCollection)
+        {
+            try
+            {
+                string _Action = string.Empty;
+                string _Controller = string.Empty;
+                string value = Convert.ToString(HttpContext.Session.GetString("AuthenticatedUser"));
+
+                if (ModelState.IsValid)
+                {
+                    if (string.IsNullOrEmpty(value))
+                    {
+                        IQueryable< Candidate> candidate = await _candidate.SearchCandidate(x => x.Email.Equals(objCollection.Email) && x.Password.Equals(objCollection.Password.EncodeBase64()));
+                        if (candidate.Any())
+                        {
+                            Candidate _candidate = new Candidate();
+                            _candidate = candidate.FirstOrDefault();
+                            _candidate.Password = _candidate.Password.EncodeBase64();
+                            HttpContext.Session.SetObjectAsJson("AuthenticatedUser", _candidate);
+                            _Controller = "Home";
+                            _Action = "Index";
+                        }
+                        else
+                        {
+                            TempData["Message"] = "Invalid User.";
+                            _Controller = "Account";
+                            _Action = "Login";
+                        }
+                    }
+                    else
+                    {
+                        _Controller = "Account";
+                        _Action = "Login";
+                    }
+                }
+                return RedirectToAction(_Action, _Controller, ViewBag.Alert);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message, ex.InnerException);
+            }
+            finally
+            {
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Logout()
+        {
+            try
+            {
+                foreach (var cookie in Request.Cookies.Keys)
+                {
+                    Response.Cookies.Delete(cookie);
+                }
+                HttpContext.Session.Clear();
+                return RedirectToAction("Login", "Account");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message, ex.InnerException);
+            }
+            finally
+            {
+            }
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Reset()
+        {
+            if (TempData["Message"] != null)
+            {
+                TempData["Message"] = null;
+            }
+            return PartialView("_Reset");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Reset([FromForm] ResetViewModel objCollection)
+        {
+            if (ModelState.IsValid)
+            {
+                int i = 0;
+                IQueryable<Candidate> _iqCandidate = await _candidate.SearchCandidate(e => e.Email.Equals(objCollection.Email));
+                if (_iqCandidate.Any())
+                {
+                    Candidate objCandidate = _iqCandidate.FirstOrDefault();
+                    objCandidate.Password = objCollection.Password.EncodeBase64();
+                    objCandidate.ModifiedBy = objCollection.Email;
+                    objCandidate.ModifiedOn = DateTime.Now;
+
+                    i = await _candidate.UpdateCandidate(objCandidate);
+
+                    if (i > 0)
+                        return RedirectToAction("Login", "Account");
+                    else
+                        TempData["Message"] = "An error occurred.";
+                }
+                else
+                    TempData["Message"] = "Invalid Email.";
+            }
+            return PartialView("_Reset");
+        }
+
+    }
+}
---
+++ ThinkItFast/Controllers/ExamController.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ThinkItFast.Models;
+using ThinkItFast.Repository.Interfaces;
+
+namespace ThinkItFast.Controllers
+{
+    [BasicAuthentication]
+    public class ExamController : Controller
+    {
+        private readonly ILogger<ExamController> _logger;
+        private readonly IExam< Exam> _exam;
+        private readonly IQuestion< Question> _question;
+        private readonly IResult< Result> _result;
+        public ExamController(ILogger<ExamController> logger, IExam< Exam> exam, IQuestion<Question> question, IResult< Result> result)
+        {
+            _logger = logger;
+            _exam = exam;
+            _question = question;
+            _result = result;
+        }
+
+        [HttpGet]
+        [Route("~/api/Exams")]
+        public async Task<IActionResult> Exams()
+        {
+            try
+            {
+                IEnumerable<Exam> lst = await _exam.GetExamList();
+                return Ok(lst.ToList());
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message, ex.InnerException);
+            }
+            finally
+            {
+            }
+        }
+
+        [HttpGet]
+        [Route("~/api/Exam/{ExamID?}")]
+        public async Task<IActionResult> Exam(int ExamID)
+        {
+            try
+            {
+                Exam exm = await _exam.GetExam(ExamID);
+                return Ok(exm);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message, ex.InnerException);
+            }
+            finally
+            {
+            }
+        }
+
+        [HttpGet]
+        [Route("~/api/Questions/{ExamID?}")]
+        public async Task<IActionResult> Questions(int ExamID)
+        {
+            try
+            {
+                QnA _obj = await _question.GetQuestionList(ExamID);
+                return Ok(_obj);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message, ex.InnerException);
+            }
+            finally
+            {
+            }
+        }
+
+        [HttpPost]
+        [Route("~/api/Score")]
+        public async Task<IActionResult> Score(List<Option> objRequest)
+        {
+            int i = 0;
+            bool IsCorrect = false;
+            List<Result> objList = null;
+            string _SessionID = null;
+            try
+            {
+                if (objRequest.Count > 0)
+                {
+                    _SessionID = Guid.NewGuid().ToString() + "-" + DateTime.Now;
+                    objList = new List<Result>();
+                    foreach (var item in objRequest)
+                    {
+                        if (item.AnswerID == item.SelectedOption)
+                            IsCorrect = true;
+                        else
+                            IsCorrect = false;
+
+                        Result obj = new Result()
+                        {
+                            CandidateID = item.CandidateID,
+                            ExamID = item.ExamID,
+                            QuestionID = item.QuestionID,
+                            AnswerID = item.AnswerID,
+                            SelectedOptionID = item.SelectedOption,
+                            IsCorrent = IsCorrect,
+                            SessionID = _SessionID,
+                            CreatedBy = "SYSTEM",
+                            CreatedOn = DateTime.Now
+                        };
+                        objList.Add(obj);
+                    }
+                    i = await _result.AddResult(objList);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                i = 0;
+                throw new Exception(ex.Message, ex.InnerException);
+            }
+            finally
+            {
+            }
+            return Ok(i);
+        }
+
+    }
+}
