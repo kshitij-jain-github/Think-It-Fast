0dfdedd4c09d5b488b1335ee93dd6d8dee6c84c0
Kshitij Jain <jainkshitij1530@gmail.com>
2023-09-06 13:51:38 +0530

    Run Migration To Update Database

---
+++ ThinkItFast/Migrations/20230906082058_init.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ThinkItFast.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Answer",
+                columns: table => new
+                {
+                    Sl_No = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    QuestionID = table.Column<int>(type: "int", nullable: false),
+                    ChoiceID = table.Column<int>(type: "int", nullable: false),
+                    DisplayText = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "datetime", nullable: true),
+                    ModifiedOn = table.Column<DateTime>(type: "datetime", nullable: true),
+                    CreatedBy = table.Column<string>(type: "varchar(200)", nullable: false),
+                    ModifiedBy = table.Column<string>(type: "varchar(200)", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Answer", x => x.Sl_No);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Candidate",
+                columns: table => new
+                {
+                    Sl_No = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "varchar(250)", nullable: false),
+                    Email = table.Column<string>(type: "varchar(250)", nullable: false),
+                    Phone = table.Column<string>(type: "varchar(20)", nullable: false),
+                    Candidate_ID = table.Column<string>(type: "varchar(20)", nullable: false),
+                    Roles = table.Column<string>(type: "varchar(100)", nullable: false),
+                    Password = table.Column<string>(type: "varchar(250)", nullable: false),
+                    ImgFile = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "datetime", nullable: true),
+                    ModifiedOn = table.Column<DateTime>(type: "datetime", nullable: true),
+                    CreatedBy = table.Column<string>(type: "varchar(200)", nullable: false),
+                    ModifiedBy = table.Column<string>(type: "varchar(200)", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Candidate", x => x.Sl_No);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Choice",
+                columns: table => new
+                {
+                    ChoiceID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    QuestionID = table.Column<int>(type: "int", nullable: false),
+                    DisplayText = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "datetime", nullable: true),
+                    ModifiedOn = table.Column<DateTime>(type: "datetime", nullable: true),
+                    CreatedBy = table.Column<string>(type: "varchar(200)", nullable: false),
+                    ModifiedBy = table.Column<string>(type: "varchar(200)", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Choice", x => x.ChoiceID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Exam",
+                columns: table => new
+                {
+                    ExamID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "varchar(1000)", nullable: false),
+                    FullMarks = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Duration = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "datetime", nullable: true),
+                    ModifiedOn = table.Column<DateTime>(type: "datetime", nullable: true),
+                    CreatedBy = table.Column<string>(type: "varchar(200)", nullable: false),
+                    ModifiedBy = table.Column<string>(type: "varchar(200)", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Exam", x => x.ExamID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Question",
+                columns: table => new
+                {
+                    QuestionID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    QuestionType = table.Column<int>(type: "int", nullable: false),
+                    DisplayText = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ExamID = table.Column<int>(type: "int", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "datetime", nullable: true),
+                    ModifiedOn = table.Column<DateTime>(type: "datetime", nullable: true),
+                    CreatedBy = table.Column<string>(type: "varchar(200)", nullable: false),
+                    ModifiedBy = table.Column<string>(type: "varchar(200)", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Question", x => x.QuestionID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Result",
+                columns: table => new
+                {
+                    Sl_No = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SessionID = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CandidateID = table.Column<int>(type: "int", nullable: false),
+                    ExamID = table.Column<int>(type: "int", nullable: false),
+                    QuestionID = table.Column<int>(type: "int", nullable: false),
+                    AnswerID = table.Column<int>(type: "int", nullable: false),
+                    SelectedOptionID = table.Column<int>(type: "int", nullable: false),
+                    IsCorrent = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "datetime", nullable: true),
+                    ModifiedOn = table.Column<DateTime>(type: "datetime", nullable: true),
+                    CreatedBy = table.Column<string>(type: "varchar(200)", nullable: false),
+                    ModifiedBy = table.Column<string>(type: "varchar(200)", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Result", x => x.Sl_No);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Answer");
+
+            migrationBuilder.DropTable(
+                name: "Candidate");
+
+            migrationBuilder.DropTable(
+                name: "Choice");
+
+            migrationBuilder.DropTable(
+                name: "Exam");
+
+            migrationBuilder.DropTable(
+                name: "Question");
+
+            migrationBuilder.DropTable(
+                name: "Result");
+        }
+    }
+}
