a2a0e988a2f8ba6ea978ff467ab2ab604d67db62
Kshitij Jain <jainkshitij1530@gmail.com>
2023-09-06 13:22:13 +0530

    Add Services

--- ThinkItFast/Extensions/ServiceCollectionExtension.cs
+++ ThinkItFast/Extensions/ServiceCollectionExtension.cs
@@ -1,7 +1,20 @@
-﻿namespace ThinkItFast.Extensions
+﻿using ThinkItFast.Models;
+using ThinkItFast.Repository.Base;
+using ThinkItFast.Repository.Interfaces;
+
+namespace ThinkItFast.Extensions
 {
-    public class ServiceCollectionExtension
+    public static class ServiceCollectionExtension
     {
 
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            return services
+           .AddScoped<ICandidate<Candidate>, CandidateService<Candidate>>()
+           .AddScoped<IExam<Exam>, ExamService<Exam>>()
+           .AddScoped<IQuestion<Question>, QuestionService<Question>>()
+           .AddScoped<IResult<Result>, ResultService<Result>>();
+
+        }
     }
 }
--- ThinkItFast/Program.cs
+++ ThinkItFast/Program.cs
@@ -1,8 +1,21 @@
+using DinkToPdf.Contracts;
+using DinkToPdf;
+using Microsoft.Extensions.Configuration;
+using ThinkItFast.Controllers;
+using ThinkItFast;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+var connection = builder.Configuration.GetConnectionString("QuizDBConnection");
+ builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession();
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+ builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connection));
+builder.Services.AddSingleton<BasicAuthentication>();
+builder.Services.AddSingleton(typeof(IConverter), new SynchronizedConverter(new PdfTools()));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -14,12 +27,13 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseSession();
 app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
